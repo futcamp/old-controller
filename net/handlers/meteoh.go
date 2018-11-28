84da81095dab66c00732a4a10a652c3c859a8b96
LittleBuster <DenisovS21@gmail.com>
2018-11-29 00:07:31 +0700

    Restructure

--- net/handlers/meteoh.go
+++ net/handlers/meteoh.go
@@ -1,3 +1,20 @@
+/*******************************************************************/
+/*
+/* Future Camp Project
+/*
+/* Copyright (C) 2018 Sergey Denisov.
+/*
+/* Written by Sergey Denisov aka LittleBuster (DenisovS21@gmail.com)
+/* Github: https://github.com/LittleBuster
+/*	       https://github.com/futcamp
+/*
+/* This library is free software; you can redistribute it and/or
+/* modify it under the terms of the GNU General Public Licence 3
+/* as published by the Free Software Foundation; either version 3
+/* of the Licence, or (at your option) any later version.
+/*
+/*******************************************************************/
+
 package handlers
 
 import (
